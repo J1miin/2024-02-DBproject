47ef84ab810912a306bb8be6d4faf1b951701540
Jimin <gift040404@naver.com>
2024-11-26 14:27:17 +0900

    [refactor] security 기본 설정 변경
    전체적으로 permitall하고, 사용자 페이지만 승인 받도록 수정

--- bbungeobbang/src/main/java/com/example/bbungeobbang/Config/SecurityConfig.java
+++ bbungeobbang/src/main/java/com/example/bbungeobbang/Config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.example.bbungeobbang.Config;
 
 import com.example.bbungeobbang.Service.CustomUserDetailsService;
+import jakarta.servlet.DispatcherType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,21 +31,20 @@ public class SecurityConfig {
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().and()
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register", "/login").permitAll()
-                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
-                        .anyRequest().authenticated()
+        http.csrf(AbstractHttpConfigurer::disable)
+//                .csrf().disable()
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/{userId}/main").hasRole("USER")
+//                                .requestMatchers("/register", "/login", "/{userId}/letter/write","/home").permitAll()  // 해당 경로는 인증 없이 접근 가능
+//                                .requestMatchers("/", "/auth/**", "/js/**", "/css/**", "/images/**").permitAll()
+                                .anyRequest().anonymous()
                 )
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .loginProcessingUrl("/login")
-                        .defaultSuccessUrl("/") // 로그인 성공 후 이동 페이지
-                )
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/login") // 로그아웃 성공 후 이동 페이지
-                );
+                    .formLogin(form -> form
+                            .loginPage("/login")
+                            .loginProcessingUrl("/login")
+                            .defaultSuccessUrl("/") // 로그인 성공 후 이동 페이지
+                            .permitAll()
+                    );
         return http.build();
     }
 
