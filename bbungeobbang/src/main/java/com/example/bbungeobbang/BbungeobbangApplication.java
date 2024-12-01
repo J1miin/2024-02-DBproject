5b297939efc50e33b9d79b72fa1d18b5c59b1d71
Jimin <88357100+J1miin@users.noreply.github.com>
2024-12-01 19:37:28 +0900

    Add files via upload

---
+++ bbungeobbang/src/main/java/com/example/bbungeobbang/BbungeobbangApplication.java
@@ -0,0 +1,13 @@
+package com.example.bbungeobbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbungeobbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbungeobbangApplication.class, args);
+	}
+
+}
