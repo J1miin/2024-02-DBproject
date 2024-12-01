7fa72e4be5ea487ebd3fe55665b36b3c8830ca4d
Jimin <gift040404@naver.com>
2024-12-01 14:49:20 +0900

    [refactor] style + 알림 내림차순 정렬

--- bbungeobbang/src/main/java/com/example/bbungeobbang/Repository/AlarmRepository.java
+++ bbungeobbang/src/main/java/com/example/bbungeobbang/Repository/AlarmRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Integer> {
-    @Query("SELECT al.alarmContents FROM Alarm al WHERE al.user.userId = :userId")
+    @Query("SELECT al.alarmContents FROM Alarm al WHERE al.user.userId = :userId ORDER BY al.letterId DESC")
     List<String> findAlarmContentsByUserId(@Param("userId") String userId);
     @Query("SELECT al.isBaked from Alarm al where al.user.userId=:userId")
     List<Boolean> findIsBakedByUserId(@Param("userId") String userId);
